67e7161ba11ecab762b8d08143a61798f43698ea
ashoordoushow <ashoor12323@gmail.com>
2024-09-18 14:13:38 -0500

    modern javascript

---
+++ index.js
@@ -0,0 +1,16 @@
+var moment = require('moment'); // require
+import { sqrt } from 'mathjs';
+
+
+console.log('heelo from jsad;sfkja;lsdfkjalsdkfj;alsdfjaasdfadsfasd;f');
+
+console.log(moment().format('MMMM Do YYYY, h:mm:ss a'));
+
+
+
+console.log(sqrt(4).toString()); // 2i
+
+console.log("actualize cohort")
+
+console.log('k dskjcn')
+ 
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+// webpack.config.js
+module.exports = {
+  mode: 'development',
+  entry: './index.js',
+  output: {
+    filename: 'main.js',
+    path: __dirname + '/dist', // Note: Use path to specify output directory
+    publicPath: 'dist/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/, // Apply Babel to JavaScript files
+        exclude: /node_modules/, // Exclude node_modules folder
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'] // Use Babel preset for ES2015+ features
+          }
+        }
+      }
+    ]
+  }
+};
+  
